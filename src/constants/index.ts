2e7937bde834433a306bc8e66bf7912f5c37c522
Rachit <rachitheretostudy@gmail.com>
2025-04-09 18:11:26 +0530

    Initial commit

---
+++ src/constants/index.ts
@@ -0,0 +1,71 @@
+export const sidebarLinks = [
+    {
+      imgURL: "/assets/icons/home.svg",
+      route: "/",
+      label: "Home",
+    },
+    {
+      imgURL: "/assets/icons/wallpaper.svg",
+      route: "/explore",
+      label: "Explore",
+    },
+    {
+        label: "YouTube",
+        route: "/youtube",
+        imgURL: "/assets/icons/youtube.png", 
+      },
+    {
+        label: "Genshin",
+        route: "https://genshin-frontend-mo45.onrender.com/",
+        imgURL: "/assets/icons/genshin-impact.png", 
+      },
+    {
+      imgURL: "/assets/icons/people.svg",
+      route: "/all-users",
+      label: "People",
+    },
+    {
+      imgURL: "/assets/icons/bookmark.svg",
+      route: "/saved",
+      label: "Saved",
+    },
+    {
+      imgURL: "/assets/icons/gallery-add.svg",
+      route: "/create-post",
+      label: "Create Post",
+    },
+  ];
+
+  export const bottombarLinks = [
+    {
+      imgURL: "/assets/icons/home.svg",
+      route: "/",
+      label: "Home",
+    },
+    {
+      imgURL: "/assets/icons/wallpaper.svg",
+      route: "/explore",
+      label: "Explore",
+    },
+    {
+      label: "Genshin",
+      route: "https://genshin-frontend-mo45.onrender.com/",
+      imgURL: "/assets/icons/genshin-impact.png", 
+    },
+    {
+        label: "YouTube",
+        route: "/youtube",
+        imgURL: "/assets/icons/youtube.svg", // Make sure this file exists
+      },
+    {
+      imgURL: "/assets/icons/bookmark.svg",
+      route: "/saved",
+      label: "Saved",
+    },
+    {
+      imgURL: "/assets/icons/gallery-add.svg",
+      route: "/create-post",
+      label: "Create",
+    },
+  ];
+  
