d2dc7def00aae9deadb94b838c7778d0e2bd21d0
Rachit <rachitheretostudy@gmail.com>
2025-04-09 18:19:30 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/', // Set to '/' for root deployments
   plugins: [react()],
   resolve: {
     alias: {
