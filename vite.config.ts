9867c6b773e0b45f8bb588b8dfc63c494fdf53c4
Rachit Sharma <156568387+Git1709@users.noreply.github.com>
2025-04-09 19:17:36 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,16 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/', // Set to '/' for root deployments
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  preview: {
+    port: 4173, // optional, just makes it explicit
+    host: true, // needed for Render
+    allowedHosts: ['genshinwuwabackend.onrender.com'], // allow your render host
   },
 });
